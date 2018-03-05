8202b1d1343b15b1137ec3232a24354085d9d14a
Maria Serna <mafesernaarboleda@gmail.com>
2018-03-05 14:46:12 -0500

    schema localitation and picture

--- api/hotel/hotel.model.js
+++ api/hotel/hotel.model.js
@@ -5,15 +5,24 @@
 
 const mongoose = require('mongoose');
 
+const PictureSchema = new mongoose.Schema({
+  url: { type: String, required: true },
+}, { _id: false });
+
+const LocalitationSchema = new mongoose.Schema({
+  lat: { type: Number, required: true },
+  log: { type: Number, required: true },
+}, { _id: false });
+
 const HotelSchema = new mongoose.Schema({
   name: { type: String, required: true },
   address: { type: String, uppercase: true },
   price: { type: Number, required: true },
   stars: { type: Number, required: true },
-  loc: { type: { lat: String, log: String }, index: { type: '2dsphere', sparse: true } },
+  loc: { type: { LocalitationSchema }, index: { type: '2dsphere', sparse: true } },
   phoneNumber: { type: String },
   about: { type: String, required: true },
-  pictures: { type: [{ url: String }] },
+  pictures: [PictureSchema],
 }, { timestamps: true });
 
 module.exports = mongoose.model('Hotel', HotelSchema);
