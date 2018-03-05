45742a2833eb35fd3b18e4cbe7f3bad067f27feb
Maria Serna <mafesernaarboleda@gmail.com>
2018-03-05 12:17:12 -0500

    delete message

--- routes.js
+++ routes.js
@@ -27,6 +27,6 @@ module.exports = (app) => {
   // All other routes should redirect to the index.html
   app.route('/*')
     .get((req, res) => {
-      res.send('puta');
+      res.json({ api: 'V1.0', description: 'API HOTELS'});
     });
 };
