a1ad57185338f2f37475781e7b9e01c51b2fa15f
Maria Serna <mafesernaarboleda@gmail.com>
2018-03-05 14:51:55 -0500

    fixed type loc

--- api/hotel/hotel.model.js
+++ api/hotel/hotel.model.js
@@ -9,17 +9,12 @@ const PictureSchema = new mongoose.Schema({
   url: { type: String, required: true },
 }, { _id: false });
 
-const LocalitationSchema = new mongoose.Schema({
-  lat: { type: Number, required: true },
-  log: { type: Number, required: true },
-}, { _id: false });
-
 const HotelSchema = new mongoose.Schema({
   name: { type: String, required: true },
   address: { type: String, uppercase: true },
   price: { type: Number, required: true },
   stars: { type: Number, required: true },
-  loc: { type: { LocalitationSchema }, index: { type: '2dsphere', sparse: true } },
+  loc: { type: [Number], index: { type: '2dsphere', sparse: true } },
   phoneNumber: { type: String },
   about: { type: String, required: true },
   pictures: [PictureSchema],
